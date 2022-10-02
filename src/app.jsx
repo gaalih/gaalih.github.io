ab55a0985c8800aecfb198be43d0807e82519046
gaalih <galih@mail.unpas.ac.id>
2022-10-02 23:01:48 +0700

    styling

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,76 @@
 import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import "./app.css";
+import Navbar from "./components/Navbar";
+// import { Icon } from "@iconify/react";
+// import { Logo } from "./assets/undraw_programming.svg";
+import Logo from "./assets/undraw_programming.svg";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div> */}
-      <h1 className="text-xl text-slate-700 font-extrabold font-mono tracking-widest">
-        Hello, My Name is Galih
-      </h1>
-      <div class="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-sky-500 px-3 py-2 shadow-md text-xs font-medium text-white"
-        >
-          count is {count}
-        </button>
+    // <div className="w-screen">
+    //   <div className="mx-20 mt-10 bg-gradient-to-tl transition duration-300 from-cyan-500 to-cyan-300 rounded-2xl px-10 py-20 h-96 shadow-xl shadow-slate-900">
+    //     <div className="text-5xl font-extrabold">
+    //       <span className="bg-clip-text text-transparent bg-gradient-to-r from-stone-600 to-slate-700">
+    //         Hello, <span className="font-extralight">My Name is</span> Galih
+    //       </span>
+    //     </div>
+    //     <div className="card">
+    //       <button
+    //         onClick={() => setCount((count) => count + 1)}
+    //         className="bg-sky-500 px-3 py-2 shadow-md text-xs font-medium text-white"
+    //       >
+    //         count is {count}
+    //       </button>
+    //     </div>
+    //     <p className="read-the-docs">This website created using vite+preact</p>
+    //   </div>
+    // </div>
+    <div className="relative w-full">
+      <Navbar />
+      <div className="relative bg-gradient-to-tl from-cyan-400 to-cyan-100">
+        <div className="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7">
+          <div className="flex items-center flex-wrap px-2 md:px-0">
+            <div className="relative lg:w-6/12 lg:py-16 xl:py-24 animate__animated animate__bounceIn">
+              <h1 className="font-bold text-4xl bg-clip-text text-transparent bg-gradient-to-tl from-slate-900 to-teal-600 md:text-5xl">
+                <span className="text-sm font-medium tracking-wide block mb-3">
+                  <b className="font-bold text-xl">Hello,</b> My name is
+                </span>
+                <b className="text-6xl tracking-wide">Galih Riyadi</b>
+                <p className="text-sm font-medium mt-2 tracking-wide">
+                  and i'am a frontend developer
+                </p>
+              </h1>
+
+              <p className="text-sm tracking-wide mt-8 text-teal-800 lg:w-10/12">
+                Saya merupakan seorang frontend developer yang antusias di
+                bidang industri kreatif seperti fotografi,videografi,desain
+                grafis. mempunyai skill dan pengalaman di bidang pemrograman
+                khususnya web programming
+              </p>
+            </div>
+            <div className="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
+              {/* <img
+                src="https://tailus.io/sources/blocks/food-delivery/preview/images/food.webp"
+                className="relative"
+                alt="food illustration"
+                loading="lazy"
+                width="4500"
+                height="4500"
+              /> */}
+              {/* <Icon icon="" /> */}
+              <img
+                src={Logo}
+                className="relative animate__animated animate__pulse"
+                alt="food illustration"
+                loading="lazy"
+                width="4500"
+                height="4500"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <p class="read-the-docs">This website created using vite+preact</p>
-    </>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { render } from "preact";
 import { App } from "./app";
-import "./index.css";
+// import "./index.css";
 import "./css/style.css";
 
 render(<App />, document.getElementById("app"));
