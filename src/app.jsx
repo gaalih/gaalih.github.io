55ce607f2c23aa697821de1f5c0c2a1842367b62
gaalih <galih@mail.unpas.ac.id>
2022-10-02 13:41:09 +0700

    edit

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" class="logo" alt="Vite logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
-      </div>
-      <h1>Vite + Preact</h1>
+      </div> */}
+      <h1 className="text-xl text-slate-700 font-extrabold font-mono tracking-widest">
+        Hello, My Name is Galih
+      </h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-sky-500 px-3 py-2 shadow-md text-xs font-medium text-white"
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <p class="read-the-docs">This website created using vite+preact</p>
     </>
-  )
+  );
 }
