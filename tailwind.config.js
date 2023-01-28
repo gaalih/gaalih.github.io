0ed2ee76187694a2293dfaa5ca0f80d3d807e5b6
gaalih <galih@mail.unpas.ac.id>
2023-01-28 17:49:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html", "./assets/js/main.js"],
+  theme: {
+    extend: {
+      inset: {
+        100: "100%",
+      },
+    },
+  },
+  plugins: [],
+};
