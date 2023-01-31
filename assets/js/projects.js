2741dfc4c98bad94c50695804f0cdca6027725d5
gaalih <galih@mail.unpas.ac.id>
2023-02-01 01:48:34 +0700

    styling, projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
       id: "modal-elisa",
       judul: "Elisa",
       deskripsi: "Aplikasi monitoring listrik dan Air",
+      imgCover: "assets/img/projects/elisa/cover.webp",
       images: [
         "assets/img/projects/elisa/001.webp",
         "assets/img/projects/elisa/002.webp",
@@ -19,6 +20,7 @@ $(document).ready(function () {
       judul: "Microgrid",
       deskripsi:
         "Aplikasi monitoring distribusi energi dalam suatu area jaringan listrik",
+      imgCover: "assets/img/projects/microgrid/cover.webp",
       images: [
         "assets/img/projects/microgrid/001.webp",
         "assets/img/projects/microgrid/002a.webp",
@@ -29,12 +31,65 @@ $(document).ready(function () {
       ],
       teknologi: "ReactJS, NextJS, TailwindCSS, Typescript",
     },
+    {
+      id: "modal-ekontrak",
+      judul: "E-Kontrak",
+      deskripsi: "Aplikasi pembuatan dokumen kontrak yang terintegrasi",
+      imgCover: "assets/img/projects/ekontrak/ekontrak-cover.webp",
+      images: ["assets/img/projects/ekontrak/ekontrak-001.webp"],
+      teknologi: "php, Codeigniter, Twig Template Engine, Oracle",
+    },
+    {
+      id: "modal-esurvey",
+      judul: "E-Survey",
+      deskripsi: "Aplikasi survey dan pengajuan barang dari katalog vendor",
+      imgCover: "assets/img/projects/esurvey/esurvey-cover.webp",
+      images: ["assets/img/projects/esurvey/esurvey-001.webp"],
+      teknologi: "php, Codeigniter, Rest API, Oracle",
+    },
+    {
+      id: "modal-pemaketanunit",
+      judul: "Pemaketan Unit",
+      deskripsi: "Modul pemaketan unit kerja di aplikasi SIMAJIK ITB",
+      imgCover: "assets/img/projects/pemaketanunit/pemaketanunit-cover.webp",
+      images: ["assets/img/projects/pemaketanunit/pemaketanunit-001.webp"],
+      teknologi: "php, Codeigniter, Oracle",
+    },
+    {
+      id: "modal-listrikmagnet",
+      judul: "Elearning",
+      deskripsi: "Aplikasi elearning pembelajaran materi listrik magnet",
+      imgCover: "assets/img/projects/listrikmagnet/listrikmagnet-cover.webp",
+      images: [
+        "assets/img/projects/listrikmagnet/listrikmagnet-001.webp",
+        "assets/img/projects/listrikmagnet/listrikmagnet-002.webp",
+      ],
+      teknologi: "php, Codeigniter, MySQL, Twig Template Engine",
+    },
   ];
 
   $.each(projects, function (key, value) {
-    $("#section-projects").append(`
+    // var imagesA = "";
+    // for (let i = 0; i < value.images.length; i++) {
+    //   imagesA.append(
+    //     `<div class="carousel-cell"><img src="${value.images[i]}" class="object-cover w-full h-full rounded-xl mb-3" /></div>`
+    //   );
+    // }
+    // `<div class="carousel-cell"><img src="${value}" class="object-cover w-full h-full rounded-xl mb-3" /></div>`;
+
+    // var dd;
+    // dd += `<div class="carousel">`;
+    // for (let index = 0; index < value.images.length; index++) {
+    //   // dd += `<img src="${value.images[index]}"/>`;
+    //   dd += ` <img src="${value.images[index]}" class="object-cover w-full h-full rounded-xl mb-3" />`;
+    //   // dd += ` <img src="${value.images[index]}" class="object-cover w-full h-full rounded-xl mb-3" />`;
+    // }
+    // dd += `</div>`;
+
+    $("#section-projects").append(
+      `
     <card key="${key}" data-target="#${value.id}" class="btn-modal relative h-60 sm:h-52 w-full sm:w-1/4 rounded-xl bg-sky-400 p-1 shadow-xl shadow-sky-500/20 scale-95 hover:scale-100 transition-all ease-in-out duration-500">
-            <img src="${value.images[0]}" class="object-cover w-full h-full rounded-xl" />
+            <img src="${value.imgCover}" class="object-cover w-full h-full rounded-xl" />
             <div class="absolute w-full h-full bg-transparent z-20 top-0 left-0 rounded-xl"></div>
             <div class="absolute z-10 w-full h-1/2 bottom-0 left-0 bg-sky-500/50 backdrop-blur-lg rounded-xl flex flex-col items-center justify-center text-center rounded-t-none">
                 <p class="text-sm sm:text-xs px-10 text-slate-700 mt-1 max-h-28 mb-2">
@@ -51,22 +106,25 @@ $(document).ready(function () {
               
               <div id="${value.id}" class="modal w-full h-full hidden">
                 <div class="w-full h-full bg-sky-400/10 backdrop-blur-lg top-0 left-0 right-0 bottom-0 fixed z-40"></div>
-                <div class="sm:w-[385px] sm:min-w-[40vw] min-w-[80vw] min-h-[50vh] flex-col items-center gap-2 -translate-y-1/2 p-6 bg-white shadow-2xl rounded-lg top-1/2 left-1/2 -translate-x-1/2 fixed flex z-50 animate-fadeIn">
+                <div class="sm:w-[70rem] sm:min-w-[40vw] min-w-[80vw] min-h-[50vh] flex-col items-center gap-2 p-6 bg-white shadow-2xl rounded-lg top-24 lg:top-5 lg:bottom-5 left-1/2 -translate-x-1/2 fixed flex z-50 animate-fadeIn">
                   <button class="close-modal w-10 h-10 rounded-full p-2 absolute z-40 -top-2 -right-2 bg-red-400 text-white font-bold shadow-md shadow-red-500/20 scale-95 hover:scale-100 transition-all ease-in-out duration-300">
                     X
                   </button>
                   <div class="flex flex-col justify-between min-h-[52vh]">
-                    <div>
-                      <img src="${value.images[1]}" class="object-cover w-full h-full rounded-xl mb-3" />
+                    <div class="flex gap-2 sm:flex-row flex-col">
+                    <img src="${value.images[0]}" class="sm:w-4/5 w-full h-auto max-h-[31rem] rounded-xl mb-3" />
+                    <div class="sm:w-1/5 w-full">
                       <span class="text-2xl font-medium text-left">${value.judul}</span>
-                      <p class="text-left">Deskripsi : <br/>${value.deskripsi}</p>
-                      <p class="text-left mt-3 text-sky-500">Teknologi : <br/>${value.teknologi}</p>
+                        <p class="text-left">Deskripsi : <br/>${value.deskripsi}</p>
+                        <p class="text-left mt-3 text-sky-500">Teknologi : <br/>${value.teknologi}</p>
+                    <button class="close-modal p-2 bg-red-400 rounded-lg w-4/5 text-white self-center mt-10 text-xs">close</button>
                     </div>
-                    <button class="close-modal p-3 bg-red-400 rounded-lg w-1/2 text-white self-center sm:mt-5">close</button>
+                    </div>
                   </div>
                 </div>
               </div>
-    `);
+    `
+    );
   });
 
   //   $("#section-projects .flex").html(`
